71e8c7117fe045ca6095a249ffdb9d7432bfc4c7
gilangbbe <gilangbbe@gmail.com>
2024-10-10 08:50:11 +0700

    fix webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -41,6 +41,29 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
     new CleanWebpackPlugin(),
 
@@ -48,8 +71,6 @@ module.exports = {
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
-
-    new BundleAnalyzerPlugin(),
 
     new CopyWebpackPlugin({
       patterns: [
--- webpack.dev.js
+++ webpack.dev.js
@@ -18,4 +18,5 @@ module.exports = merge(common, {
     watchFiles: ['src/**/*'],
     compress: true,
   },
+  plugins: [new BundleAnalyzerPlugin()],
 });
